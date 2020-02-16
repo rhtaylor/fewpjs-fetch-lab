0d8d58f93fe6d013bcb20ea7c12ed0f4844bced7
Randy <randyisbusy@gmail.com>
2020-02-15 19:59:30 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function fetchBooks() {
-
+  fetch('https://anapioficeandfire.com/api/books').then(function (response) {
+    return response.json();
+  }).then(function (json) {
+      console.log(json) 
+      renderBooks(json)
+    });
 }
 
 function renderBooks(json) {
